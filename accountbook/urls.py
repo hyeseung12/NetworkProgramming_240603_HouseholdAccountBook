0d67b59485d1d76e304d2bd424868088a2eb44b9
MsEmily1020 <peace10200khs@gmail.com>
2024-06-24 10:45:03 +0900

    list url 변경

--- accountbook/urls.py
+++ accountbook/urls.py
@@ -6,7 +6,7 @@ app_name = 'accountbook'
 
 urlpatterns = [
    path('category/', views.CategoryListView.as_view(), name='category_list'),
-   path('accountbook/', views.AccountBookListView.as_view(), name='accountbook_list'),
+   path('', views.AccountBookListView.as_view(), name='accountbook_list'),
    path('create/', views.AccountBookCreateView.as_view(), name='accountbook_create'),
    path('update/<int:pk>/', views.AccountBookUpdateView.as_view(), name='accountbook_update'),
    path('delete/<int:pk>/', views.AccountBookDeleteView.as_view(), name='accountbook_delete'),
